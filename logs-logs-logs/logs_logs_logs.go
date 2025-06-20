279266d56c808c07db872faa55bc857b18d11ead
logan1o1 <saswatm2706@gmail.com>
2025-06-19 22:42:28 -0700

    runes/utf8 unicodes

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,40 @@
 package logs
 
+import (
+	"unicode/utf8"
+)
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, l := range log {
+		switch l {
+		case rune(0x2757):
+			return "recommendation"
+		case rune(0x1F50D):
+			return "search"
+		case rune(0x2600):
+			return "weather"
+		}
+	}
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	var newLog []rune
+	for _, l := range log {
+		if l != oldRune {
+			newLog = append(newLog, l)
+		} else {
+			newLog = append(newLog, newRune)
+		}
+	}
+	return string(newLog)
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return utf8.RuneCountInString(log) <= limit
 }
