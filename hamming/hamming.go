680ea26b9fdb256989cb5efc6d5850b804cca84e
logan1o1 <saswatm2706@gmail.com>
2025-07-11 09:04:38 -0700

    hammering

---
+++ hamming/hamming.go
@@ -0,0 +1,19 @@
+package hamming
+
+import "errors"
+
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("not equal strings")
+	}
+
+	var count int
+
+	for i, _ := range a {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+
+	return count, nil
+}
